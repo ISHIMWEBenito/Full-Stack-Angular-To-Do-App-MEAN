73750522b5555f54a73573c85fbcd135e573c312
BenitoIshimwe <benishimwe31@gmail.com>
2023-02-14 20:21:05 +0100

    Add Backend Url

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -1,40 +1,41 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv/config");
 
 //router imports
-const userRoutes = require('./routes/UserRoute');
-const todoRoutes = require('./routes/TaskRoute');
+const userRoutes = require("./routes/UserRoute");
+const todoRoutes = require("./routes/TaskRoute");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/users', userRoutes);
-app.use('/api/todo', todoRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/todo", todoRoutes);
 
-app.get('', (req, res) => {
+app.get("", (req, res) => {
   res.status(200).json({
-    message: 'Hello World!',
+    message: "Hello World!",
   });
 });
 
-app.listen(3000, (err) => {
+app.listen(4000, (err) => {
   if (err) {
     console.log(err);
   }
-  console.log('Server is running at http://localhost:3000');
+  console.log(`Server is running at http://localhost:${process.env.PORT}`);
 });
 
 mongoose.connect(
-  'mongodb+srv://pms:s7YlRpQHNGFV6XMM@cluster0.yijgg.mongodb.net/tododb?retryWrites=true&w=majority',
+  process.env.DB_CONNECTION,
   { useUnifiedTopology: true, useNewUrlParser: true },
   (err) => {
     if (err) {
       console.log(err);
     }
 
-    console.log('Database connected');
+    console.log("Database connected");
   }
 );
