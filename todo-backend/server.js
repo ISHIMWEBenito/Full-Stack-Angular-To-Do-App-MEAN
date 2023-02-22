2b509f0646b5a4cd55702826b5fe70579931d6dd
BenitoIshimwe <benishimwe31@gmail.com>
2023-02-22 11:26:15 +0100

    Add new port

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -29,7 +29,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, (err) => {
   if (err) {
     console.log(err);
